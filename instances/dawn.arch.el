88d68f8e7d88131d1393bfb18f3c37af850dbc17
Fernanda Guimarães <fernandaguimaraes28@gmail.com>
2019-09-07 18:53:15 -0300

    Merge branch gaghag-master.

--- instances/dawn.arch.el
+++ instances/dawn.arch.el
@@ -15,7 +15,8 @@
                              feature-python
                              feature-rust
                              feature-docker
-                             feature-ein))
+                             feature-ein
+                             feature-org-reveal))
 
 (load dotemacs-file)
 
@@ -35,7 +36,7 @@
     ("9aa31b4b4b84965818055d3fb80ffc2efa2a8d4ec068e202bd26a8da26415bd5" "a8810eb3e7166f6d1c5825d6de963c45d6d9ea5eb037728c2ff2e0349c4a3a28" default)))
  '(package-selected-packages
    (quote
-    (spinner wakatime-mode lsp-java rust-mode undo-tree rainbow-mode helm-spotify-plus latex-preview-pane togetherly htmlize ledger-mode transpose-frame multiple-cursors ace-jump-mode expand-region intero lsp-python projectile cquery pdf-tools ox-reveal calfw-org calfw dired-quick-sort dired-ranger magit company-lsp lsp-ui lsp-mode flycheck use-package try powerline helm doom-themes company))))
+    (org-re-reveal sml-mode spinner wakatime-mode lsp-java rust-mode undo-tree rainbow-mode helm-spotify-plus latex-preview-pane togetherly htmlize ledger-mode transpose-frame multiple-cursors ace-jump-mode expand-region intero lsp-python projectile cquery pdf-tools ox-reveal calfw-org calfw dired-quick-sort dired-ranger magit company-lsp lsp-ui lsp-mode flycheck use-package try powerline helm doom-themes company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
